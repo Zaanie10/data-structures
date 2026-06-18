d0ef1e07730a37205dbc6dfd0580a59ef985e786
Zaanie <79721198+Zaanie10@users.noreply.github.com>
2026-06-18 00:05:55 -0400

    Clean up comments in CheckPalindrome.java
    Removed unnecessary comments and metadata from CheckPalindrome.java.

--- Stacks/CheckPalindrome.java
+++ Stacks/CheckPalindrome.java
@@ -1,10 +1,3 @@
-// Name:    Zaanie Bowen
-// Class:   CS3305/W03
-// Term:    Summer 2025
-// Instructor:   Prof. Wang
-// Assignment:   #3
-// IDE Name:   IntelliJ IDEA
-
 import java.util.Scanner;
 
 public class CheckPalindrome {
