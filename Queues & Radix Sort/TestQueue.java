505e7177051a4bea8a35625d7397631e268f238a
Zaanie <79721198+Zaanie10@users.noreply.github.com>
2026-06-18 00:05:41 -0400

    Clean up header comments in TestQueue.java
    Removed unnecessary comments from TestQueue.java.

--- Queues & Radix Sort/TestQueue.java
+++ Queues & Radix Sort/TestQueue.java
@@ -1,10 +1,3 @@
-// Name: Zaanie Bowen
-// Class: CS3305/W03
-// Term: Summer 2025
-// Instructor: Prof. Wang
-// Assignment: 4
-// IDE Name: IntelliJ
-
 import java.util.Scanner;
 
 public class TestQueue {
